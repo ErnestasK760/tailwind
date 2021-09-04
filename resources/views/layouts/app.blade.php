ec57a2eda4bb2d4a3bbf173fca2293c82e5dd31f
ErnestasK760 <82801762+ErnestasK760@users.noreply.github.com>
2021-09-04 17:44:41 +0300

    Updated nav
    Removed decoration fixing hamburger

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,21 +27,22 @@
 <body>
     <div id="app">
         <header class="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2">
-            <div class="flex-1 flex justify-between items-center">
-               <img class="h-8" src="{{ asset('images/credit.svg')}}" alt="Credit">
-          </div>
+            <div class="flex-1 flex items-center">
+               <img class="h-12 pr-3" src="{{ asset('images/credit.svg')}}" alt="Credit">
+                 <span class="text-base">   
+                    Ernesto Bankas
+                </span>
+            </div>
         
            <label for="menu-toggle" class="pointer-cursor sm:hidden block"><i class="text-2xl fas fa-bars"></i></label>
           <input class="hidden" type="checkbox" id="menu-toggle" />
         
           <div class="hidden sm:flex sm:items-center sm:w-auto w-full" id="menu">
             <nav>
-              <ul class="sm:flex items-center justify-between text-base text-gray-700 pt-4 sm:pt-0">
-                <li><a class="lg:p-4 py-3 px-1 block border-b-2 border-transparent hover:no-underline hover:border-indigo-400" href="#">Features</a></li>
-                <li><a class="lg:p-4 py-3 px-1 block border-b-2 border-transparent hover:no-underline hover:border-indigo-400" href="#">Pricing</a></li>
-                <li><a class="lg:p-4 py-3 px-1 block border-b-2 border-transparent hover:no-underline hover:border-indigo-400" href="#">Documentation</a></li>
-                <li><a class="lg:p-4 py-3 px-1 block border-b-2 border-transparent hover:no-underline hover:border-indigo-400" href="#">Support</a></li>
-                <a href="#" class="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor">
+              <ul class="sm:flex items-center justify-between text-base text-gray-700 sm:pt-0">
+                <li><a class="lg:p-4 py-3 px-2 block border-b-2 border-transparent hover:no-underline hover:border-indigo-400" href="#">Accounts</a></li>
+                <li><a class="lg:p-4 py-3 px-2 block border-b-2 border-transparent hover:no-underline hover:border-indigo-400" href="#">Clients</a></li>
+                <a href="#" class="ml-3 flex justify-start pointer-cursor">
                     <img class="rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400" src="{{ asset('images/avatar.svg')}}" alt="Andy Leverenz">
                 </a>
               </ul>
